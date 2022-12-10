2c33d73d21f514d9c00839c7f594aa1c6a776192
admacha <119713764+admacha@users.noreply.github.com>
2022-12-10 15:12:50 -0600

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --opt-split-attention --medvram 
-set 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:3072'
+set COMMANDLINE_ARGS= --xformers --opt-split-attention 
+
+::--listen::
+
+::--medvram::
+
 call webui.bat
